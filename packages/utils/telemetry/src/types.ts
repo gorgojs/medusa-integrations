356fa1fab8756b7c98d70ae31916abfd6d0fddcc
Ghost1863 <al.malin2006@gmail.com>
2026-08-03 20:52:56 +0300

    feat(telemetry): remove extra telemetry

--- packages/utils/telemetry/src/types.ts
+++ packages/utils/telemetry/src/types.ts
@@ -7,7 +7,6 @@ export interface EnvInfo {
   medusa_version: string
   node_version: string
   os: string
-  arch: string
   ci: boolean
   container: boolean
   node_env: string
@@ -17,7 +16,6 @@ export interface EnvInfo {
   package_manager: "npm" | "yarn" | "pnpm" | "unknown"
 
   store_id?: string
-  admin_id?: string
 }
 
 export interface TelemetryEvent {
