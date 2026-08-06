850f904d365ba76ae027fc463458fff82b8eeeaf
KaneFlak <kflak@yandex.ru>
2026-08-06 14:43:20 +0300

    feat(integration): add admin i18n translations for 31 locales

--- packages/modules/integration/src/admin/i18n/index.ts
+++ packages/modules/integration/src/admin/i18n/index.ts
@@ -1,7 +1,69 @@
+import ar from "./json/ar.json" with { type: "json" }
+import bg from "./json/bg.json" with { type: "json" }
+import bs from "./json/bs.json" with { type: "json" }
+import cs from "./json/cs.json" with { type: "json" }
+import de from "./json/de.json" with { type: "json" }
+import el from "./json/el.json" with { type: "json" }
 import en from "./json/en.json" with { type: "json" }
+import enGB from "./json/enGB.json" with { type: "json" }
+import es from "./json/es.json" with { type: "json" }
+import fa from "./json/fa.json" with { type: "json" }
+import fr from "./json/fr.json" with { type: "json" }
+import he from "./json/he.json" with { type: "json" }
+import hr from "./json/hr.json" with { type: "json" }
+import hu from "./json/hu.json" with { type: "json" }
+import id from "./json/id.json" with { type: "json" }
+import it from "./json/it.json" with { type: "json" }
+import ja from "./json/ja.json" with { type: "json" }
+import ko from "./json/ko.json" with { type: "json" }
+import lt from "./json/lt.json" with { type: "json" }
+import mk from "./json/mk.json" with { type: "json" }
+import mn from "./json/mn.json" with { type: "json" }
+import nl from "./json/nl.json" with { type: "json" }
+import pl from "./json/pl.json" with { type: "json" }
+import ptBR from "./json/ptBR.json" with { type: "json" }
+import ptPT from "./json/ptPT.json" with { type: "json" }
+import ro from "./json/ro.json" with { type: "json" }
 import ru from "./json/ru.json" with { type: "json" }
+import th from "./json/th.json" with { type: "json" }
+import tr from "./json/tr.json" with { type: "json" }
+import uk from "./json/uk.json" with { type: "json" }
+import vi from "./json/vi.json" with { type: "json" }
+import zhCN from "./json/zhCN.json" with { type: "json" }
+import zhTW from "./json/zhTW.json" with { type: "json" }
 
 export default {
+  ar: { translation: ar },
+  bg: { translation: bg },
+  bs: { translation: bs },
+  cs: { translation: cs },
+  de: { translation: de },
+  el: { translation: el },
   en: { translation: en },
+  enGB: { translation: enGB },
+  es: { translation: es },
+  fa: { translation: fa },
+  fr: { translation: fr },
+  he: { translation: he },
+  hr: { translation: hr },
+  hu: { translation: hu },
+  id: { translation: id },
+  it: { translation: it },
+  ja: { translation: ja },
+  ko: { translation: ko },
+  lt: { translation: lt },
+  mk: { translation: mk },
+  mn: { translation: mn },
+  nl: { translation: nl },
+  pl: { translation: pl },
+  ptBR: { translation: ptBR },
+  ptPT: { translation: ptPT },
+  ro: { translation: ro },
   ru: { translation: ru },
+  th: { translation: th },
+  tr: { translation: tr },
+  uk: { translation: uk },
+  vi: { translation: vi },
+  zhCN: { translation: zhCN },
+  zhTW: { translation: zhTW },
 }
