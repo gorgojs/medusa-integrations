dcb321b81efe0e101963108d4b7b8338d8b73266
KaneFlak <kflak@yandex.ru>
2026-08-11 14:47:40 +0300

    fix: add and update custom injection zones

--- packages/providers/payment-robokassa/index.d.ts
+++ packages/providers/payment-robokassa/index.d.ts
@@ -2,7 +2,7 @@ export {}
 
 declare module "@medusajs/admin-shared" {
   interface InjectionZoneRegistry {
-    "gorgo.integration.apiship": true,
-    "gorgo.integration.apiship.side": true
+    "gorgo.integration.robokassa": true,
+    "gorgo.integration.robokassa.side": true
   }
 }
